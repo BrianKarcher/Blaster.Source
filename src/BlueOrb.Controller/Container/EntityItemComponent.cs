4dc8a4874a3d4960fc6c694e5dc9aab4efd7830e
Brian Karcher <bkarcher@colony-games.com>
2020-12-11 17:41:15 -0800

    EntityItem Component.

---
+++ src/BlueOrb.Controller/Container/EntityItemComponent.cs
@@ -0,0 +1,11 @@
+﻿using BlueOrb.Common.Components;
+using UnityEngine;
+
+namespace BlueOrb.Controller.Block
+{
+    [AddComponentMenu("RQ/Components/Entity Item")]
+    public class EntityItemComponent : ComponentBase<EntityItemComponent>
+    {
+
+    }
+}
