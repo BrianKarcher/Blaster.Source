188d93c40f0fde07a21e4140cc5cd9a59a099f37
Brian Karcher <bkarcher@colony-games.com>
2022-07-20 01:25:54 -0700

    Heal player buff.

---
+++ src/BlueOrb.Controller/Buff/HealPlayer.cs
@@ -0,0 +1,20 @@
+﻿using BlueOrb.Common.Container;
+using BlueOrb.Messaging;
+using UnityEngine;
+
+namespace BlueOrb.Controller.Buff
+{
+    [AddComponentMenu("BlueOrb/Buff/HealPlayer")]
+    public class HealPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private string notificationMessage = "HEALED";
+
+        private void Start()
+        {
+            EntityContainer.Instance.LevelStateController.SetCurrentHp(EntityContainer.Instance.LevelStateController.GetMaxHp());
+            MessageDispatcher.Instance.DispatchMsg("Notification", 0f, null, "Hud Controller", notificationMessage);
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
