7c8221872b4ae688dfc2cea84ab4ffb20d3d34ff
Brian Karcher <bkarcher@colony-games.com>
2022-07-25 23:56:19 -0700

    Item acquire trigger updates.

--- src/BlueOrb.Controller/Inventory/ItemAcquireTrigger.cs
+++ src/BlueOrb.Controller/Inventory/ItemAcquireTrigger.cs
@@ -23,23 +23,48 @@ namespace BlueOrb.Controller.Inventory
         [SerializeField]
         private bool destroyOnTrigger = true;
 
+        [SerializeField]
+        private string projectileHitMessage = "ProjectileHit";
+
+        private long projectileHitId;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.attachedRigidbody != null ? other.attachedRigidbody.CompareTag(targetTag) : other.CompareTag(targetTag))
             {
-                MessageDispatcher.Instance.DispatchMsg(InventoryComponent.AddItemMessage, 0f, null, LevelStateController.Id,
-                    new ItemDesc
-                    {
-                        ItemConfig = this.itemConfig,
-                        Qty = this.qty
-                    }
-                );
-                if (this.destroyOnTrigger)
-                {
-                    // Destroy immediate so no further trigger entries count
-                    GameObject.DestroyImmediate(base._componentRepository.gameObject);
-                }
+                Trigger();
             }
+        }
+
+        private void Trigger()
+        {
+            MessageDispatcher.Instance.DispatchMsg(InventoryComponent.AddItemMessage, 0f, null, LevelStateController.Id,
+                new ItemDesc
+                {
+                    ItemConfig = this.itemConfig,
+                    Qty = this.qty
+                }
+            );
+            if (this.destroyOnTrigger)
+            {
+                // Destroy immediate so no further trigger entries count
+                GameObject.DestroyImmediate(base._componentRepository.gameObject);
+            }
+        }
+
+        public override void StartListening()
+        {
+            base.StartListening();
+            projectileHitId = MessageDispatcher.Instance.StartListening(projectileHitMessage, base._componentRepository.GetId(), (data) =>
+            {
+                Trigger();
+            });
+        }
+
+        public override void StopListening()
+        {
+            base.StopListening();
+            MessageDispatcher.Instance.StopListening(this.projectileHitMessage, base._componentRepository.GetId(), this.projectileHitId);
         }
     }
 }
