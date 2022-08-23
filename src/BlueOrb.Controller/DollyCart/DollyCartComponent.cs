48d9570827d25bac1b6bf9fe4c56f3773f57ee07
Brian Karcher <bkarcher@colony-games.com>
2022-08-22 20:40:47 -0700

    Transition to DollyCart FSM

--- src/BlueOrb.Controller/DollyCart/DollyCartComponent.cs
+++ src/BlueOrb.Controller/DollyCart/DollyCartComponent.cs
@@ -1,4 +1,5 @@
-﻿using BlueOrb.Common.Components;
+﻿using BlueOrb.Base.Attributes;
+using BlueOrb.Common.Components;
 using BlueOrb.Messaging;
 using Cinemachine;
 using UnityEngine;
@@ -13,12 +14,14 @@ namespace BlueOrb.Controller
         [SerializeField]
         private CinemachineDollyCart _cinemachineDollyCart;
         [SerializeField]
-        public LerpType _speedChangeType;
+        public LerpType _speedChangeType = LerpType.SmoothDamp;
         [SerializeField]
         private GameObject _dollyJoint;
 
         [SerializeField]
         private float _targetSpeed;
+        public float GetTargetSpeed => _targetSpeed;
+
         [SerializeField]
         private float _startSpeed;
         [SerializeField]
@@ -26,12 +29,33 @@ namespace BlueOrb.Controller
 
         private long _setSpeedTargetId, _setSpeedId, _setCineCart, _setTrack;
         [SerializeField]
-        private bool _updatingSpeed;
+        private bool _updatingSpeed = false;
         private float _velocity;
         [SerializeField]
         private float _smoothTime = 2f;
+        public float SmoothTime => _smoothTime;
+
         [SerializeField]
         private bool _speedDecreasing = false;
+        [SerializeField]
+        private float emergencyBrakeTime = 1.0f;
+
+        [SerializeField]
+        [Tag]
+        private string enemyTag;
+        public string EnemyTag => this.enemyTag;
+
+        //[SerializeField]
+        ////[Layer]
+        //private int[] EnemyLayer;
+
+        [SerializeField]
+        private Vector3 enemyCheckOffset = new Vector3(0f, 0f, 2f);
+        public Vector3 EnemyCheckOffset => this.enemyCheckOffset;
+
+        [SerializeField]
+        private Vector3 enemyCheckHalfExtents = new Vector3(0.5f, 1f, 1f);
+        public Vector3 EnemyCheckHalfExtents => this.enemyCheckHalfExtents;
 
         private GameObject cinemachineDollyCartGameObject;
         private Vector3 oldDollyCartPosition;
@@ -65,7 +89,8 @@ namespace BlueOrb.Controller
         //    _cinemachineDollyCart = dolly.GetComponent<CinemachineDollyCart>();
         //}
 
-        public float GetSpeed() => _cinemachineDollyCart.m_Speed;
+        public float GetSpeed() => cinemachineDollyCartGameObject == null ? 0 : _cinemachineDollyCart.m_Speed;
+        private float SetSpeed(float speed) => _cinemachineDollyCart.m_Speed = speed;
 
         //[SerializeField]
         //private InventoryData _inventoryData;
@@ -80,57 +105,13 @@ namespace BlueOrb.Controller
             //ItemsByType = new Dictionary<ItemTypeEnum, ItemDesc>();
         }
 
-        private void Update()
-        {
-            if (this.cinemachineDollyCartGameObject == null)
-            {
-                return;
-            }
-            // Directly translate the position and rotation based on the delta for the Dolly Cart.
-            Vector3 distanceDelta = this.cinemachineDollyCartGameObject.transform.position - this.oldDollyCartPosition;
-            //Quaternion rotationDelta = this.oldDollyCartRotation * Quaternion.Inverse(this.oldDollyCartRotation);
-            //Quaternion rotationDelta = this._dollyJoint.transform.rotation.to - this.oldDollyCartRotation;
-            //Vector3 forwardDelta = this._dollyJoint.transform.forward - this.oldDollyCartRotation;
-            //Quaternion newQ = rotationDelta + this._dollyJoint.transform.rotation;
-            //_dollyJoint.transform.Translate()
-            //Mathf.MoveTowardsAngle()
-
-            //if (distanceDelta.sqrMagnitude < Mathf.)
-            //{
-            //    return;
-            //}
-            _dollyJoint.transform.localPosition += distanceDelta;
-
-            //Short explanation: targetAngle - myAngle + 540 calculates targetAngle -myAngle + 180 and adds 360 to ensure it's a positive number,
-            //since compilers can be finicky about % modulus with negative numbers. Then % 360 normalizes the difference to [0, 360).
-            //And finally the - 180 subtracts the 180 added at the first step, and shifts the range to [-180, 180).
-            float deltaYaw = (this.cinemachineDollyCartGameObject.transform.localEulerAngles.y - this.oldyaw + 540) % 360 - 180;
-            yaw += deltaYaw;
-            float deltaPitch = (this.cinemachineDollyCartGameObject.transform.localEulerAngles.x - this.oldpitch + 540) % 360 - 180;
-            pitch += deltaPitch;
-            _dollyJoint.transform.eulerAngles = new Vector3(pitch, yaw, 0);
-            SetOldPositionAndRotation(this.cinemachineDollyCartGameObject);
-        }
-
         public override void StartListening()
         {
             base.StartListening();
             _setSpeedTargetId = MessageDispatcher.Instance.StartListening("SetSpeedTarget", _componentRepository.GetId(), (data) =>
             {
-                _updatingSpeed = true;
                 var speedData = data.ExtraInfo as SetSpeedData;
-                _startSpeed = _cinemachineDollyCart.m_Speed;
-                _currentSpeed = _startSpeed;
-                _smoothTime = speedData.SmoothTime;
-                _targetSpeed = speedData.TargetSpeed;
-                if (_startSpeed < _targetSpeed)
-                {
-                    _speedDecreasing = false;
-                }
-                else
-                {
-                    _speedDecreasing = true;
-                }
+                StartAcceleration(speedData);
             });
 
             _setSpeedId = MessageDispatcher.Instance.StartListening("SetSpeed", _componentRepository.GetId(), (data) =>
@@ -138,7 +119,7 @@ namespace BlueOrb.Controller
                 var speed = (float)data.ExtraInfo;
                 _startSpeed = speed;
                 _currentSpeed = speed;
-                _cinemachineDollyCart.m_Speed = speed;
+                SetSpeed(speed);
             });
 
             _setCineCart = MessageDispatcher.Instance.StartListening("SetCineCart", _componentRepository.GetId(), (data) =>
@@ -156,6 +137,25 @@ namespace BlueOrb.Controller
             //    Debug.Log($"Adding item {item.ItemConfig.name}, qty {item.Qty}");
             //    //AddItem(item);
             //});
+        }
+
+        public void Brake() => StartAcceleration(new SetSpeedData { TargetSpeed = 0, SmoothTime = this.emergencyBrakeTime });
+
+        public void StartAcceleration(SetSpeedData speedData)
+        {
+            _updatingSpeed = true;
+            _startSpeed = GetSpeed();
+            _currentSpeed = _startSpeed;
+            _smoothTime = speedData.SmoothTime;
+            _targetSpeed = speedData.TargetSpeed;
+            if (_startSpeed < _targetSpeed)
+            {
+                _speedDecreasing = false;
+            }
+            else
+            {
+                _speedDecreasing = true;
+            }
         }
 
         public void SetDollyCart(GameObject dollyCart, bool resetCartPosition = true)
@@ -192,41 +192,86 @@ namespace BlueOrb.Controller
         public void RotateTo(Vector3 dir, float time)
         {
             Debug.Log($"Rotating to {dir}");
-            this._cinemachineDollyCart.m_Speed = 0f;
+            SetSpeed(0f);
             gameObject.transform.rotation = Quaternion.Euler(dir.x, dir.y, dir.z);
-            this._cinemachineDollyCart.m_Speed = 5f;
+            SetSpeed(5f);
             //iTween.RotateTo(gameObject, dir, time);
         }
 
         protected void FixedUpdate()
         {
-            if (_updatingSpeed)
-            {
-                switch (_speedChangeType)
-                {
-                    case LerpType.Lerp:
-                        _cinemachineDollyCart.m_Speed = Mathf.Lerp(_cinemachineDollyCart.m_Speed, _targetSpeed, 1f / _smoothTime * Time.deltaTime);
-                        //_cinemachineDollyCart.m_Speed = Mathf.Lerp(_startSpeed, _targetSpeed, 1f / _smoothTime * Time.deltaTime);
-                        //_cinemachineDollyCart.m_Speed = Mathf.Lerp(_cinemachineDollyCart.m_Speed, _targetSpeed, _smoothTime * Time.deltaTime);
+            //if (_updatingSpeed)
+            //{
+            //    ProcessDollyCartSpeed();
+            //}
 
-                        _currentSpeed = _cinemachineDollyCart.m_Speed;
-                        break;
-                    case LerpType.SmoothDamp:
-                        _cinemachineDollyCart.m_Speed = UnityEngine.Mathf.SmoothDamp(_currentSpeed, _targetSpeed, ref _velocity, _smoothTime);
-                        _currentSpeed = _cinemachineDollyCart.m_Speed;
-                        break;
-                }
-                if (Mathf.Approximately(_currentSpeed, _targetSpeed) || (_speedDecreasing && _currentSpeed <= _targetSpeed + 0.01f))
-                {
-                    _currentSpeed = _targetSpeed;
-                    _updatingSpeed = false;
-                }
-                else if (Mathf.Approximately(_currentSpeed, _targetSpeed) || (!_speedDecreasing && _currentSpeed >= _targetSpeed - 0.01f))
-                {
-                    _currentSpeed = _targetSpeed;
-                    _updatingSpeed = false;
-                }
+            UpdatePositionAndRotation();
+        }
+
+        public void Stop()
+        {
+            _updatingSpeed = false;
+            SetSpeed(0f);
+        }
+
+        public void ProcessDollyCartSpeedChange()
+        {
+            switch (_speedChangeType)
+            {
+                case LerpType.Lerp:
+                    SetSpeed(Mathf.Lerp(GetSpeed(), _targetSpeed, 1f / _smoothTime * Time.deltaTime));
+                    //_cinemachineDollyCart.m_Speed = Mathf.Lerp(_startSpeed, _targetSpeed, 1f / _smoothTime * Time.deltaTime);
+                    //_cinemachineDollyCart.m_Speed = Mathf.Lerp(_cinemachineDollyCart.m_Speed, _targetSpeed, _smoothTime * Time.deltaTime);
+
+                    _currentSpeed = GetSpeed();
+                    break;
+                case LerpType.SmoothDamp:
+                    SetSpeed(Mathf.SmoothDamp(_currentSpeed, _targetSpeed, ref _velocity, _smoothTime));
+                    _currentSpeed = GetSpeed();
+                    break;
             }
+            if (Mathf.Approximately(_currentSpeed, _targetSpeed) || (_speedDecreasing && _currentSpeed <= _targetSpeed + 0.01f))
+            {
+                _currentSpeed = _targetSpeed;
+                _updatingSpeed = false;
+            }
+            else if (Mathf.Approximately(_currentSpeed, _targetSpeed) || (!_speedDecreasing && _currentSpeed >= _targetSpeed - 0.01f))
+            {
+                _currentSpeed = _targetSpeed;
+                _updatingSpeed = false;
+            }
+        }
+
+        private void UpdatePositionAndRotation()
+        {
+            if (this.cinemachineDollyCartGameObject == null)
+            {
+                return;
+            }
+            // Directly translate the position and rotation based on the delta for the Dolly Cart.
+            Vector3 distanceDelta = this.cinemachineDollyCartGameObject.transform.position - this.oldDollyCartPosition;
+            //Quaternion rotationDelta = this.oldDollyCartRotation * Quaternion.Inverse(this.oldDollyCartRotation);
+            //Quaternion rotationDelta = this._dollyJoint.transform.rotation.to - this.oldDollyCartRotation;
+            //Vector3 forwardDelta = this._dollyJoint.transform.forward - this.oldDollyCartRotation;
+            //Quaternion newQ = rotationDelta + this._dollyJoint.transform.rotation;
+            //_dollyJoint.transform.Translate()
+            //Mathf.MoveTowardsAngle()
+
+            //if (distanceDelta.sqrMagnitude < Mathf.)
+            //{
+            //    return;
+            //}
+            _dollyJoint.transform.localPosition += distanceDelta;
+
+            //Short explanation: targetAngle - myAngle + 540 calculates targetAngle -myAngle + 180 and adds 360 to ensure it's a positive number,
+            //since compilers can be finicky about % modulus with negative numbers. Then % 360 normalizes the difference to [0, 360).
+            //And finally the - 180 subtracts the 180 added at the first step, and shifts the range to [-180, 180).
+            float deltaYaw = (this.cinemachineDollyCartGameObject.transform.localEulerAngles.y - this.oldyaw + 540) % 360 - 180;
+            yaw += deltaYaw;
+            float deltaPitch = (this.cinemachineDollyCartGameObject.transform.localEulerAngles.x - this.oldpitch + 540) % 360 - 180;
+            pitch += deltaPitch;
+            _dollyJoint.transform.eulerAngles = new Vector3(pitch, yaw, 0);
+            SetOldPositionAndRotation(this.cinemachineDollyCartGameObject);
         }
     }
 }
