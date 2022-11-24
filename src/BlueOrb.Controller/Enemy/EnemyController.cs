c423e9396f62d31962be4de3ed1c4f04c2dec1fa
Brian Karcher <bkarcher@colony-games.com>
2022-11-24 01:12:17 -0800

    Enemy end life.

---
+++ src/BlueOrb.Controller/Enemy/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using BlueOrb.Base.Manager;
+using BlueOrb.Common.Components;
+using System;
+using UnityEngine;
+
+namespace BlueOrb.Controller.Enemy
+{
+    [AddComponentMenu("Blue Orb/Components/Enemy Controller")]
+    public class EnemyController : ComponentBase<EnemyController>
+    {
+        [SerializeField]
+        private float lifetimeSeconds = 60f;
+
+        private float lifeEndTime;
+        private bool lifeCountdownStarted = false;
+
+        private void OnUpdate()
+        {
+            if (!GameStateController.Instance.LevelStateController.HasLevelBegun)
+            {
+                return;
+            }
+            if (!lifeCountdownStarted)
+            {
+                this.lifeEndTime = Time.time + lifetimeSeconds;
+            }
+            if (Time.time > this.lifeEndTime)
+            {
+                this.GetComponentRepository().Destroy();
+            }
+        }
+    }
+}
