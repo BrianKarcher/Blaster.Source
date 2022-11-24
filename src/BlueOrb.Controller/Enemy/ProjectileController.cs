c1624bf58b114c1d947c372145062a91705a6b6b
Brian Karcher <bkarcher@colony-games.com>
2022-11-24 01:21:36 -0800

    Projectile Controller

--- src/BlueOrb.Controller/Enemy/ProjectileController.cs
+++ src/BlueOrb.Controller/Enemy/ProjectileController.cs
@@ -7,24 +7,14 @@ using UnityEngine;
 
 namespace BlueOrb.Controller.Enemy
 {
-    [AddComponentMenu("Blue Orb/Components/Enemy Controller")]
-    public class EnemyController : ComponentBase<EnemyController>
+    [AddComponentMenu("Blue Orb/Components/Projectile Controller")]
+    public class ProjectileController : ComponentBase<EnemyController>
     {
         [SerializeField]
         private float lifetimeSeconds = 60f;
 
-        [SerializeField]
-        private float maxDistanceFromPlayer = 300f;
-        private float maxDistanceFromPlayerSquared;
-
         private float lifeEndTime;
         private bool lifeCountdownStarted = false;
-
-        protected override void Awake()
-        {
-            base.Awake();
-            maxDistanceFromPlayerSquared = maxDistanceFromPlayer * maxDistanceFromPlayer;
-        }
 
         private void OnUpdate()
         {
@@ -37,11 +27,6 @@ namespace BlueOrb.Controller.Enemy
                 this.lifeEndTime = Time.time + lifetimeSeconds;
             }
             if (Time.time > this.lifeEndTime)
-            {
-                this.GetComponentRepository().Destroy();
-            }
-            // TODO Add a field of view check to this
-            if (Vector2.SqrMagnitude(this.GetComponentRepository().GetFootPosition().xz() - EntityContainer.Instance.GetMainCharacter().GetFootPosition().xz()) > maxDistanceFromPlayerSquared)
             {
                 this.GetComponentRepository().Destroy();
             }
